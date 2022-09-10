1a5918161f4273e7e5b2612877088f753b0c28ce
dali-ntonga <dntonga@live.com>
2022-09-09 20:00:19 -0700

    done

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,14 +3,13 @@
 #include <time.h>
 
 /**
- * * main - print if the number is postive, zero, or negative
- * * Description: using the main function
- * * this program prints "Programming is positive, zero, or negative
- * * Return 0
+ *  main - print if the number is postive, zero, or negative
+ *  Description : using the main function
+ *  this program prints "Programming is positive, zero, or negative"
+ *  Return 0
  **/
 
-int main(void)
-
+main(void)
 {
 
 	char ch;
