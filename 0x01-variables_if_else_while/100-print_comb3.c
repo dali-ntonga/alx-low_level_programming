998899e0197da71370785ef85ed1fb134b8d033a
dali-ntonga <dntonga@live.com>
2022-09-12 15:29:11 -0700

    combo fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,9 +11,9 @@ int main(void)
 	int a;
 	int b;
 
-	for (a = '0' ; a <= '9' ; a++)
+	for (a = 0 ; a <= 9 ; a++)
 	{
-		for (b = '1' ; b <= '9' ; b++)
+		for (b = 1 ; b <= 9 ; b++)
 		{
 			if (b < a)
 			{
