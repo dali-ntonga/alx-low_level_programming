948df5d60f100466970fcd0d337bd941fe0f6f0f
dali-ntonga <dntonga@live.com>
2022-09-12 14:14:27 -0700

    spaces ad commas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers with space and comma
+ * Return: always(sucess)
+ **/
+
+int main(void)
+{
+	int c;
+
+	for (c = '0' ; c <= '9' ; c++)
+	{
+		putchar (c);
+
+		if (c != '9')
+			putchar (',');
+			putchar (' ');
+	}
+	putchar('\n');
+	return (0);
+}
