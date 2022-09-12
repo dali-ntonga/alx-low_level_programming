1aa8ef40e8c0ed5cef78f130c9718bf33f4bee26
dali-ntonga <dntonga@live.com>
2022-09-12 15:24:56 -0700

    combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main -prints combination of digits
+ * Return:always(sucess)
+ **/
+
+int main(void)
+
+{
+	int a;
+	int b;
+
+	for (a = '0' ; a <= '9' ; a++)
+	{
+		for (b = '1' ; b <= '9' ; b++)
+		{
+			if (b < a)
+			{
+				putchar (a + '0');
+				putchar (b + '0');
+				if (a != 8)
+				{
+					putchar (',');
+					putchar (' ');
+				}
+			}
+		}
+	}
+	putchar ('\n');
+	return (0);
+}
