c9c7501143677fa8f28e71f20479b277303fe856
dali-ntonga <dntonga@live.com>
2022-09-12 18:40:06 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints output using puts
+ * Return: Success
+ **/
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle,");
+	return (0);
+}
