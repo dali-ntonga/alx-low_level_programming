96e20ae3fe23bf1b04c4ce82f6e46f3b2a5bbd58
dali-ntonga <dntonga@live.com>
2022-09-14 15:43:20 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if its alphabet
+ * @c: argument that passes into function
+ * Return: always 0
+ **/
+
+int _isalpha(int c)
+{
+	if (_isalpha(c) == 0)
+		return (0);
+	else
+		return (1);
+}
