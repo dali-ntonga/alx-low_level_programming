66cec5f7b13737335280d57019d33a8806b6fcd5
dali-ntonga <dntonga@live.com>
2022-09-14 17:01:34 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - calculates the absolute value
+ * @c: argument to be passed into function
+ * Return: always 0
+ **/
+
+int _abs(int c)
+{
+	if (c > 0 || C == 0)
+	{
+		return (c);
+	}
+	else
+		return (c * -1);
+}
+
