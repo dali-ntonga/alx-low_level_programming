6d98795307f711f236d4efea19ad5058aa60e937
dali-ntonga <dntonga@live.com>
2022-09-20 16:12:00 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _puts- prints a string to stdout
+ * @str: string that will be printed
+ *
+ * Description: prints a string
+ * Return:  Always 0
+ **/
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str = i != '\0')
+	{
+		putchar (*(str = i));
+		i++;
+	}
+	putchar (10);
+}
