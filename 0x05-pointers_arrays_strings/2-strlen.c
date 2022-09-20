3ceac85ca3915544fe9fa958e74c880abf1b08c7
dali-ntonga <dntonga@live.com>
2022-09-20 15:56:03 -0700

    string lenght

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen- returns length of string
+ * @s: string
+ * Return: Always 0
+ **/
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++
+	return (len);
+}
