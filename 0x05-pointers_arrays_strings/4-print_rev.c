740c68b8fa8246aaf1aaf2a47623f42095af860c
dali-ntonga <dntonga@live.com>
2022-09-20 18:33:12 -0700

    another fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -4,15 +4,13 @@
 /**
  * print_rev- prints string in reverse
  * @s: string to reverse
- * 
  * Return: Void 
  **/
-
 void print_rev(char *s)
 {
 	int len = strlen(s);
 
-	while (len --)
+	while (len--)
 		_putchar(*(s + len));
 	_putchar(10);
 }
