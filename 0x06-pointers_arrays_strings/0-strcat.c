b38310c2ae544643d21be0bf4d2efa96ce7ad714
dali-ntonga <dntonga@live.com>
2022-09-22 14:23:34 -0700

    two string concatenate

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * *_strcat -concatenates two strings
+ * @dest: string to be ammended
+ * @src: string to be copied
+ * Return: Dest
+ **/
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, dest_str = 0;
+
+	while (dest[i++])
+		dest_str++;
+	for (i = 0; src[i]; i++)
+		dest(dest_str++) = src[i];
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,15 @@
+0 - concatenates two strings "strcat" 
+
+1 - concatenates two strings using "strncat"
+
+2 - Copies string using "strncopy"
+
+3 - compares two strings using "strcmp"
+
+4 - reverses an array of integers 
+
+5 - changes all lowercase letters to uppercase 
+
+6 - Capitalises all words of a string 
+
+7 - encodes a string into 1337
