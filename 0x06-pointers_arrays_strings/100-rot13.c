4935033f682839fc9cfa61ecadc88bb6922ac983
dali-ntonga <dntonga@live.com>
2022-09-22 19:52:33 -0700

    print

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,10 @@
+#include "main.h" 
+
+/**                                                                                                                                   
+
+ * rot13 - Encodes a string using rot13.                                                                                                    
+
+ *                                                                                                                                    
+
+ * Return: A pointer to the encoded string.                                                                                           
+
