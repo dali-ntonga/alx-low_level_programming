a0e7ccf217092018edb7ee160c44cf50e9db2bf7
dali-ntonga <dntonga@live.com>
2022-09-22 17:27:16 -0700

    fixers

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h" 
+
+/**
+ * _strcmp - compares two strings
+ * @s1: string 1
+ * @s2: string 2
+ * Return:
+ * returns zero is s1 == s2
+ * returns negative number is s1 < s2
+ * returns positive number if s1 > s2
+ **/
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	return (*s1 - *s2);
+}
