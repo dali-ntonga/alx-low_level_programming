99bae5b5a5cada626c024a50b1900d1ccaf76329
dali-ntonga <dntonga@live.com>
2022-09-24 02:45:33 -0700

    betty reduced # of lines

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -4,9 +4,7 @@
  * print_times_table -> prints times table
  * @n: number of the times table
  **/
-
-void print_times_table(int n)
-{
+void print_times_table(int n) {
 	int i, j, k;
 
 	if (n >= 0 && n <= 15)
@@ -26,16 +24,14 @@ void print_times_table(int n)
 		_putchar(' ');
 		_putchar(' ');
 		_putchar(k + '0');
-	}
-	else if (k >= 10 && k < 100)
+	} else if (k >= 10 && k < 100)
 	{
 		_putchar(',');
 		_putchar(' ');
 		_putchar(' ');
 		_putchar((k / 10) + '0');
 		_putchar((k % 10) + '0');
-	}
-	else if (k >= 100)
+	} else if (k >= 100)
 	{
 		_putchar(',');
 		_putchar(' ');
@@ -43,9 +39,7 @@ void print_times_table(int n)
 		_putchar(((k / 10) % 10) + '0');
 		_putchar((k % 10) + '0');
 	}
+	}_putchar('\n');
 	}
-	_putchar('\n');
 	}
-	}
-
 }
