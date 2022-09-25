db99e1aa6f1048889adb85bcb815c5d5369d7a6d
dali-ntonga <dntonga@live.com>
2022-09-25 13:25:07 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isdigit -> checks if its a digit
+ * @c: input
+ * Return: 1 if c is a digit, else 0.
+ **/
+int _isdigit(int c)
+{
+	if ((c >= '0') && (c <= '9'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
